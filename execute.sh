ac8589c9ebdd1ddf27c8bcfd8f0dd55da9a778bd
prateek.pant@fox.com <prateek.pant@fox.com>
2022-06-06 15:25:49 +0530

    change ownership after file copy

--- execute.sh
+++ execute.sh
@@ -5,12 +5,12 @@ sudo snap install docker
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 #sudo apt install -y  nodejs
 mkdir -p /home/ubuntu/application
-sudo chown -R  ubuntu /home/ubuntu/application
 cd /home/ubuntu/application
 touch start
 aws s3 cp   s3://cpe-appserver-bucket/app_dir/docker-compose.yml  /home/ubuntu/application/docker-compose.yml
 aws s3 cp   s3://cpe-appserver-bucket/app_dir/app  /home/ubuntu/application/app  --recursive
 aws s3 cp   s3://cpe-appserver-bucket/app_dir/nginx /home/ubuntu/application/nginx --recursive
 touch stop
+sudo chown -R  ubuntu /home/ubuntu/application
 sudo docker-compose up -d --scale web=1
 
